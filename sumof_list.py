f18b11c5aabede91e4d2807a2c92144b5c1aa4a3
rambabu199 <93924576+rambabu199@users.noreply.github.com>
2021-11-21 22:42:56 +0530

    Python function to sum all the numbers in a list.
    Python function to sum all the numbers in a list.

---
+++ sumof_list.py
@@ -0,0 +1,49 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 18,
+   "id": "b1cfe698",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "20\n"
+     ]
+    }
+   ],
+   "source": [
+    "''' A Python function to sum all the numbers in a list.'''\n",
+    "list1=(8, 2, 3, 0, 7)\n",
+    "def sum_of_list(a):#single agrument function\n",
+    "    a=sum(list1)#sum is a predifined function to sum\n",
+    "    return a # returning a value\n",
+    "b=sum_of_list(list1) # assigning a value of sum to b\n",
+    "print(b) # printing B"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
